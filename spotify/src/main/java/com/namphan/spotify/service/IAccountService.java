ca5318c12e80882535044829ca09b5f2dd7cb477
caleb1123 <ryy1507@gmail.com>
2023-12-02 00:33:04 +0700

    update

--- spotify/src/main/java/com/namphan/spotify/service/IAccountService.java
+++
@@ -1,8 +0,0 @@
-package com.namphan.spotify.service;
-
-import com.namphan.spotify.dto.AccountsDTO;
-import com.namphan.spotify.entity.Account;
-
-public interface IAccountService {
-    AccountsDTO Login(String userName, String password);
-}
